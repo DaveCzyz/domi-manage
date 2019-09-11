fcffeed02aab3ff43901d3ea4d45a426c9dbff84
Dawid <dawczyz@poczta.fm>
2019-09-11 23:15:07 +0200

    Add login feature

--- app/class/User.class.php
+++ app/class/User.class.php
@@ -1,5 +1,27 @@
 <?php
 
+class User{
+    protected static $db_name = "users";
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $password;
+
+    // Static function for verifing user during login
+    public static function verifyUser($m, $p){
+        global $db;
+        $email      = $db->escape_string($m);
+        $password   = $db->escape_string($p);
+
+        $sql = "SELECT * FROM ". self::$db_name;
+        $sql.= " WHERE email='".$email."' AND password='".$password."' LIMIT 1";
+        $db->query($sql);
+        $query = $db->query($sql);
+
+        return $query->num_rows;
+    }
+
+}
 
 
 ?>
