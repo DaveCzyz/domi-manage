5ffe23be5e74e2022f04742c139273a92cafd575
Dawid <dawczyz@poczta.fm>
2019-09-16 18:10:00 +0200

    Add error message

--- app/class/Session.class.php
+++ app/class/Session.class.php
@@ -2,7 +2,15 @@
 
 class Session{
     
-
+    public static function errorMessage($msg){
+        echo "
+            <div class='row justify-content-center'>
+                <div class='alert alert-danger col-4 text-center' role='alert'>
+                    ".$msg."
+                </div>
+            </div>
+        ";
+    }
 
 
 
