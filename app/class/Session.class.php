1c8a57d91fa95b3373cf52352c4f2944de652890
Dawid <dawczyz@poczta.fm>
2019-09-21 12:42:21 +0200

    Display message from Session

--- app/class/Session.class.php
+++ app/class/Session.class.php
@@ -1,15 +1,59 @@
 <?php
 
 class Session{
+
+    public $message;
+
+    public function __construct(){
+        session_start();
+    }
+
+    public function message($msg = ""){
+        if(!empty($msg)){
+            $_SESSION['message'] = $msg;
+        }
+    }
+
+    public function displayMessage(){
+        if(isset($_SESSION['message'])){
+            $this->message = $_SESSION['message'];
+        }
+        unset($_SESSION['message']);
+    }
     
-    public static function errorMessage($msg){
-        echo "
-            <div class='row justify-content-center'>
-                <div class='alert alert-danger col-4 text-center' role='alert'>
-                    ".$msg."
-                </div>
-            </div>
-        ";
+
+    public static function throwMessage($status, $msg){
+        switch ($status){
+            case "success":
+                echo "
+                    <div class='row justify-content-center'>
+                        <div class='alert alert-success col-4 text-center' role='alert'>
+                            ".$msg."
+                        </div>
+                    </div>
+                ";
+            break;
+
+            case "error":
+                echo "
+                    <div class='row justify-content-center'>
+                        <div class='alert alert-danger col-4 text-center' role='alert'>
+                            ".$msg."
+                        </div>
+                    </div>
+                ";
+            break;
+
+            case "info" :
+                echo "
+                    <div class='row justify-content-center'>
+                        <div class='alert alert-info col-4 text-center' role='alert'>
+                            ".$msg."
+                        </div>
+                    </div>
+                ";
+            break;
+        }
     }
 
 
